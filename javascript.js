b7b4a4303a989e8e7cbae1837a8463b5440e9869
Priyanka <pbhaukajee@gmail.com>
2024-01-06 15:49:39 -0600

    add form details

---
+++ javascript.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let password = document.querySelector("#password").value;
+  let confirmPassword = document.querySelector("#confirmPassword").value;
+
+  let error = document.querySelector("#error");
+
+  if (password == confirmPassword) {
+    error.innerHTML = "";
+  }
+  console.log("hi");
+});
